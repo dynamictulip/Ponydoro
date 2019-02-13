599f8f9eb0244cc2f80aa6898b2965cca844736f
Sara Gowen <sara.gowen@ukho.gov.uk>
2019-02-13 14:24:45 +0000

    Draggable window

--- Pomo-Shiny/MainWindow.xaml.cs
+++ Pomo-Shiny/MainWindow.xaml.cs
@@ -1,18 +1,25 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using System.Windows;
+using System.Windows.Input;
 
 namespace Pomo_Shiny
 {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
+    [ExcludeFromCodeCoverage]
     public partial class MainWindow : Window
     {
-        [ExcludeFromCodeCoverage]
         public MainWindow()
         {
             DataContext = new MainWindowViewModel(new ApplicationAccessor(), new CountdownTimer());
             InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                DragMove();
         }
     }
 }
