7b86061bc9c14b7f600d1c412259d49ba39ecdd1
Sara Gowen <sara.gowen@ukho.gov.uk>
2019-02-13 10:44:05 +0000

    Implemented timer

--- Pomo-Shiny/MainWindowViewModel.cs
+++ Pomo-Shiny/MainWindowViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.Diagnostics;
+﻿using System;
 using System.Threading;
 using System.Windows.Input;
 
@@ -8,7 +7,8 @@ namespace Pomo_Shiny
     public class MainWindowViewModel : ObservableObject
     {
         private readonly IApplicationAccessor _applicationAccessor;
-        private Timer timer;
+        private TimeSpan _remainingTime;
+        private Timer _timer;
 
         public MainWindowViewModel(IApplicationAccessor applicationAccessor)
         {
@@ -16,23 +16,46 @@ namespace Pomo_Shiny
 
             ExitCommand = new DelegateCommand(Exit);
             StartTimerCommand = new DelegateCommand(StartTimer);
+            StartBreakTimerCommand = new DelegateCommand(StartBreakTimer);
+        }
+
+        public ICommand ExitCommand { get; }
+        public ICommand StartTimerCommand { get; }
+        public ICommand StartBreakTimerCommand { get; }
+
+        public TimeSpan RemainingTime
+        {
+            get => _remainingTime;
+            private set
+            {
+                _remainingTime = value;
+                RaisePropertyChangedEvent(nameof(RemainingTime));
+            }
         }
 
         private void StartTimer()
         {
-            timer = new Timer(ElapsedTime);
-
+            NewTimer(25);
         }
 
-        private void ElapsedTime(object state)
+        private void StartBreakTimer()
         {
-            throw new System.NotImplementedException();
+            NewTimer(5);
         }
 
+        private void NewTimer(int minutes)
+        {
+            _timer?.Dispose();
+            RemainingTime = new TimeSpan(0, 0, minutes, 0);
+            _timer = new Timer(UpdateRemainingTime, null, 1000, 1000);
+        }
 
-        public ICommand ExitCommand { get; }
-
-        public ICommand StartTimerCommand { get; }
+        private void UpdateRemainingTime(object state)
+        {
+            RemainingTime = RemainingTime.Subtract(new TimeSpan(0, 0, 0, 1));
+            if (RemainingTime.TotalSeconds < 1)
+                _timer.Dispose();
+        }
 
         private void Exit()
         {
