bec5269d6b2522b56ea52c4b41afdf3de3cc9382
Sara Gowen <sara.gowen@ukho.gov.uk>
2019-02-13 10:14:07 +0000

    Now using transparent background

--- Pomo-Shiny/MainWindowViewModel.cs
+++ Pomo-Shiny/MainWindowViewModel.cs
@@ -1,19 +1,38 @@
-﻿using System.Windows.Input;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Input;
 
 namespace Pomo_Shiny
 {
     public class MainWindowViewModel : ObservableObject
     {
         private readonly IApplicationAccessor _applicationAccessor;
+        private Timer timer;
 
         public MainWindowViewModel(IApplicationAccessor applicationAccessor)
         {
             _applicationAccessor = applicationAccessor;
 
             ExitCommand = new DelegateCommand(Exit);
+            StartTimerCommand = new DelegateCommand(StartTimer);
         }
 
+        private void StartTimer()
+        {
+            timer = new Timer(ElapsedTime);
+
+        }
+
+        private void ElapsedTime(object state)
+        {
+            throw new System.NotImplementedException();
+        }
+
+
         public ICommand ExitCommand { get; }
+
+        public ICommand StartTimerCommand { get; }
 
         private void Exit()
         {
