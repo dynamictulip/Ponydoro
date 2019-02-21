fad7dd4a08d08823caa63080b07350274dc12e05
Sara Gowen <sara.gowen@ukho.gov.uk>
2019-02-21 07:38:23 +0000

    Added BoolToVisibilityConverter to simplify turning components on and off Also started playing with the context menu

---
+++ Pomo-Shiny/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Pomo_Shiny.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            var isVisible = value != null && (bool)value;
+            return isVisible ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class InverseBoolToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            var isVisible = value != null && (bool)value;
+            return !isVisible ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
